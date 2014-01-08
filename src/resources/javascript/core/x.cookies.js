77a18c591b5d17fdb6a2f8fcb8d4b2f2f203072b
yoshow <ruanyu@live.com>
2014-01-08 10:44:13 +0800

    add x.cookies.js x.css.js

---
+++ src/resources/javascript/core/x.cookies.js
@@ -0,0 +1,61 @@
+﻿// -*- ecoding : utf-8 -*-
+
+//=============================================================================
+//
+// Copyright (c) 2010 ruanyu@live.com
+//
+// FileName     :x.cookies.js
+//
+// Description  :
+//
+// Author       :Max
+//
+// Date         :2010-01-01
+//
+//=============================================================================
+
+define(['jquery', 'x'], function($, x)
+{
+    x.cookies = {
+
+        find: function(name)
+        {
+            var value = '';
+            var search = name + '=';
+
+            if (document.cookie.length > 0)
+            {
+                var offset = document.cookie.indexOf(search);
+
+                if (offset != -1)
+                {
+                    offset += search.length;
+
+                    var end = document.cookie.indexOf(';', offset);
+
+                    if (end == -1)
+                        end = document.cookie.length;
+
+                    value = unescape(document.cookie.substring(offset, end));
+                }
+            }
+
+            return value;
+        },
+
+        add: function(name, value, expire, path, domain)
+        {
+            document.cookie = name + '=' + escape(value) + ((!expire) ? '' : ('; expires=' + expire.toGMTString())) + '; path=' + ((!path) ? '/' : path) + ((!domain) ? '' : ('; domain=' + domain));
+        },
+
+        remove: function(name, path, domain)
+        {
+            if (x.cookies.find(name))
+            {
+                document.cookie = name + '=' + '; path=' + ((!path) ? '/' : path) + '; expires=' + new Date(0).toGMTString() + ((!domain) ? '' : ('; domain=' + domain)); ;
+            }
+        }
+    };
+
+    return x.cookies;
+});
---
+++ src/resources/javascript/core/x.css.js
@@ -0,0 +1,86 @@
+﻿// -*- ecoding : utf-8 -*-
+
+define(['jquery', 'x'], function($, x)
+{
+    x.css = {
+
+        /*#region 函数:style(element)*/
+        /**
+        * 获取样式信息
+        */
+        style: function(element)
+        {
+            return element.currentStyle || window.getComputedStyle(element, null);
+        },
+        /*#endregion*/
+
+        /*#region 函数:check(element, className)*/
+        /**
+        * @description 检测 css className 是否存在.
+        *
+        * @param {string} element
+        * @param {string} className
+        */
+        check: function(element, className)
+        {
+            /*
+            var found=false;
+            var temparray=o.className.split(' ');
+
+            for(var i=0;i<temparray.length;i++){
+            if(temparray[i]==c1){found=true;}
+            }
+            return found;
+            */
+
+            var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+
+            return element.className.match(reg) == null ? false : true;
+        },
+        /*#endregion*/
+
+        /*#region 函数:swap(element, classNameA, classNameB)*/
+        /**
+        * 替换 css className
+        */
+        swap: function(element, classNameA, classNameB)
+        {
+            if (this.check(element, classNameA))
+            {
+                element.className = element.className.replace(classNameA, classNameB);
+            }
+        },
+        /*#endregion*/
+
+        /*#region 函数:add(element, className)*/
+        /**
+        * 添加 css className
+        */
+        add: function(element, className)
+        {
+            if (!this.check(element, className))
+            {
+                element.className += ' ' + className;
+                element.className = element.className.trim();
+            }
+        },
+        /*#endregion*/
+
+        /*#region 函数:remove(element, className)*/
+        /**
+        * 移除 css className
+        */
+        remove: function(element, className)
+        {
+            if (this.check(element, className))
+            {
+                var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+                element.className = element.className.replace(reg, '');
+                element.className = element.className.trim();
+            }
+        }
+        /*#endregion*/
+    };
+
+    return x.css;
+});
